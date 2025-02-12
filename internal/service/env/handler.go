3c583e3ffcf3fab3c22b12527e80a31508d7b90b
ambitious <1535820771@qq.com>
2025-02-12 14:32:41 +0800

    perf: add env search button

--- internal/service/env/handler.go
+++ internal/service/env/handler.go
@@ -18,3 +18,17 @@ func StoreEnv(c *gin.Context) {
 	Set(key, value)
 	c.String(http.StatusOK, "设置成功")
 }
+
+// FindEnv 查询环境变量
+func FindEnv(c *gin.Context) {
+	key := c.Query("key")
+	if strs.AnyEmpty(key) {
+		c.String(http.StatusBadRequest, "参数不足")
+		return
+	}
+	if value, ok := Get(key); ok {
+		c.String(http.StatusOK, value)
+		return
+	}
+	c.String(http.StatusNotFound, "环境变量不存在")
+}
