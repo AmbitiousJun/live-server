f685c414cd1b2a92b21aa1e44c65a80cb51429ae
ambitious <1535820771@qq.com>
2025-03-24 15:42:59 +0800

    perf: optimize combine response logic

--- internal/service/resolve/handler/combine.go
+++ internal/service/resolve/handler/combine.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"math/rand/v2"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/AmbitiousJun/live-server/internal/constant"
 	"github.com/AmbitiousJun/live-server/internal/service/env"
@@ -12,17 +14,59 @@ import (
 	"github.com/AmbitiousJun/live-server/internal/util/strs"
 )
 
+// combineChCache 聚合频道缓存
+type combineChCache struct {
+
+	// idx 频道索引缓存
+	idx int
+
+	// createTime 缓存创建时间
+	createTime time.Time
+}
+
 // combineHandler 接口聚合处理器
 type combineHandler struct {
+
+	// caches 缓存
+	caches map[string]combineChCache
+
+	// cacheTimeout 频道缓存过期时间
+	cacheTimeout time.Duration
+
+	// cacheOpMutex 控制缓存并发读写
+	cacheOpMutex sync.RWMutex
 
 	// chSeg 分割多个直播源的分隔符
 	chSeg string
 }
 
 func init() {
-	resolve.RegisterHandler(&combineHandler{
-		chSeg: constant.HeadersSeg,
-	})
+	y := &combineHandler{
+		chSeg:        constant.HeadersSeg,
+		cacheTimeout: time.Second * 10,
+		caches:       make(map[string]combineChCache),
+	}
+	resolve.RegisterHandler(y)
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		for range ticker.C {
+			if len(y.caches) == 0 {
+				continue
+			}
+
+			y.cacheOpMutex.Lock()
+			toDeletes := make([]string, 0)
+			for k, v := range y.caches {
+				if time.Since(v.createTime) > y.cacheTimeout {
+					toDeletes = append(toDeletes, k)
+				}
+			}
+			for _, del := range toDeletes {
+				delete(y.caches, del)
+			}
+			y.cacheOpMutex.Unlock()
+		}
+	}()
 }
 
 // Handle 处理直播, 返回一个用于重定向的远程地址
@@ -42,10 +86,17 @@ func (h *combineHandler) Handle(params resolve.HandleParams) (resolve.HandleResu
 		return resolve.HandleResult{}, fmt.Errorf("直播源列表为空, 请使用 %s 分隔多个直播源", h.chSeg)
 	}
 
-	// 3 随机选择一个直播源
+	// 3 缓存检测
+	if cacheIdx, ok := h.readCacheIdx(params.UrlEnv); ok && cacheIdx < len(chs) && cacheIdx >= 0 {
+		return resolve.HandleResult{Type: resolve.ResultRedirect, Url: chs[cacheIdx]}, nil
+	}
+
+	// 4 随机选择一个直播源
+	randIdx := rand.IntN(len(chs))
+	h.putCacheIdx(params.UrlEnv, randIdx)
 	return resolve.HandleResult{
 		Type: resolve.ResultRedirect,
-		Url:  chs[rand.IntN(len(chs))],
+		Url:  chs[randIdx],
 	}, nil
 }
 
@@ -82,4 +133,23 @@ func (h *combineHandler) SupportCustomHeaders() bool {
 // Enabled 标记处理器是否是启用状态
 func (h *combineHandler) Enabled() bool {
 	return true
+}
+
+// readCacheIdx 读取缓存中的频道索引
+func (h *combineHandler) readCacheIdx(env string) (int, bool) {
+	h.cacheOpMutex.RLock()
+	defer h.cacheOpMutex.RUnlock()
+
+	if cache, ok := h.caches[env]; ok {
+		return cache.idx, true
+	}
+
+	return 0, false
+}
+
+// putCacheIdx 写入频道索引缓存
+func (h *combineHandler) putCacheIdx(env string, idx int) {
+	h.cacheOpMutex.Lock()
+	defer h.cacheOpMutex.Unlock()
+	h.caches[env] = combineChCache{idx: idx, createTime: time.Now()}
 }
