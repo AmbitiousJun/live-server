bce1702889ef3993d6f30874511262f46c55346f
ambitious <1535820771@qq.com>
2025-04-01 15:49:02 +0800

    perf(aktv): auto detect ad

--- internal/service/resolve/handler/aktv.go
+++ internal/service/resolve/handler/aktv.go
@@ -1,12 +1,15 @@
 package handler
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"strings"
 	"time"
 
 	"github.com/AmbitiousJun/live-server/internal/service/resolve"
 	"github.com/AmbitiousJun/live-server/internal/util/base64s"
+	"github.com/AmbitiousJun/live-server/internal/util/colors"
 	"github.com/AmbitiousJun/live-server/internal/util/https"
 )
 
@@ -14,6 +17,8 @@ func init() {
 	h := &aktvHandler{
 		host:       base64s.MustDecodeString("aHR0cHM6Ly9ha3R2LnNwYWNl"),
 		errorTsSeg: "error",
+		adTsSeg:    "/ad",
+		adErr:      errors.New("ad"),
 		cc:         https.NewCacheClient(1, time.Hour*2),
 	}
 	h.m3uAddr = h.host + "/live.m3u"
@@ -32,6 +37,12 @@ type aktvHandler struct {
 
 	// errorTsSeg 频道异常的 ts 切片地址片段
 	errorTsSeg string
+
+	// adTsSeg ad ts 切片地址片段
+	adTsSeg string
+
+	// adErr 检测 ad
+	adErr error
 
 	// cc 缓存订阅信息的 http 客户端
 	cc *https.CacheClient
@@ -52,12 +63,25 @@ func (h *aktvHandler) Handle(params resolve.HandleParams) (resolve.HandleResult,
 	}
 
 	// 3 代理原始的 m3u 文本, 检查频道是否失效
-	content, err := resolve.ProxyM3U(chInfo.Url, nil, false, resolve.ModeCustom, params.ClientHost)
-	if err != nil {
-		return resolve.HandleResult{}, fmt.Errorf("检查频道有效性失败: %v", err)
+	curTry, maxTry := 1, 5
+	for curTry <= maxTry {
+		err := h.contentValid(chInfo.Url, params.ClientHost)
+		if err == nil {
+			break
+		}
+
+		if errors.Is(err, h.adErr) {
+			log.Printf(colors.ToGray("handler: aktv, waiting ad..., curTry: %d, maxTry: %d"), curTry, maxTry)
+			curTry++
+			resolve.RemoveM3UProxyCache(chInfo.Url)
+			time.Sleep(time.Second * 2)
+			continue
+		}
+
+		return resolve.HandleResult{}, fmt.Errorf("[%s] 失效: %v", params.ChName, err)
 	}
-	if strings.Contains(content, h.errorTsSeg) {
-		return resolve.HandleResult{}, fmt.Errorf("频道异常: [%s], 请稍后再试", params.ChName)
+	if curTry > maxTry {
+		return resolve.HandleResult{}, fmt.Errorf("尝试获取 [%s] 频道次数过多, 请稍后再试", params.ChName)
 	}
 
 	return resolve.M3U8Result(chInfo.Url, params)
@@ -94,4 +118,19 @@ func (h *aktvHandler) SupportCustomHeaders() bool {
 // Enabled 标记处理器是否是启用状态
 func (h *aktvHandler) Enabled() bool {
 	return true
+}
+
+// contentValid 检查频道是否有效
+func (h *aktvHandler) contentValid(chUrl, clientHost string) error {
+	content, err := resolve.ProxyM3U(chUrl, nil, false, resolve.ModeCustom, clientHost)
+	if err != nil {
+		return fmt.Errorf("检查频道有效性失败: %v", err)
+	}
+	if strings.Contains(content, h.errorTsSeg) {
+		return errors.New("频道异常")
+	}
+	if strings.Contains(content, h.adTsSeg) {
+		return h.adErr
+	}
+	return nil
 }
